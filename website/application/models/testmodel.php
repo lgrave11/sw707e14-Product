45da5fecdf5cd32a2c668d64e74b60f0969e5d92
Dennis Jakobsen <dennis.jakobsen@gmail.com>
2014-09-30 09:47:43 +0200

    Tilføjet MVC struktur

---
+++ website/application/models/testmodel.php
@@ -0,0 +1,25 @@
+<?php
+
+class StatsModel
+{
+    /**
+     * Every model needs a database connection, passed to the model
+     * @param object $db A PDO database connection
+     */
+    function __construct($db) {
+        try {
+            $this->db = $db;
+        } catch (Exception $e) {
+            exit('Database connection could not be established.');
+        }
+    }
+
+    /**
+     * Get simple "stats". This is just a simple demo to show
+     * how to use more than one model in a controller (see application/controller/songs.php for more)
+     */
+    public function someStuff()
+    {
+        
+    }
+}
---
+++ website/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * A simple PHP MVC skeleton
+ *
+ * @package php-mvc
+ * @author Panique
+ * @link http://www.php-mvc.net
+ * @link https://github.com/panique/php-mvc/
+ * @license http://opensource.org/licenses/MIT MIT License
+ */
+
+// load application config (error reporting etc.)
+require 'application/config/config.php';
+
+// load application class
+require 'application/libs/application.php';
+require 'application/libs/controller.php';
+
+// start the application
+$app = new Application();
