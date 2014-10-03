8a332cc5017d8d5f25cab0be46d487f4e08aed81
Søren Skibsted Als <s_s_a_92@hotmail.com>
2014-10-03 08:37:35 +0200

    lidt mere for pullsor

--- website/application/models/bookingfactory.php
+++ website/application/models/bookingfactory.php
@@ -1,5 +1,15 @@
 <?php
 class BookingFactory
 	
+	$db = null;
+
+	function __construct($database){
+		try{
+			$this->db = $database;
+		}
+		catch(Exception $ex){
+			exit("Unable to connect to database " . $ex);
+		}
+	}
 
 ?>
