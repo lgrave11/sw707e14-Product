7924ac60899640f70333355bed7f7d94a52ddae9
Winde <mapede11@student.aau.dk>
2014-10-06 12:57:13 +0200

    En ny home, der åbner googleapi

--- website/application/controller/home.php
+++ website/application/controller/home.php
@@ -22,7 +22,7 @@ class Home extends Controller
 
         // load views. within the views we can echo out $songs and $amount_of_songs easily
         require 'application/views/_templates/header.php';
-        require 'application/views/home/index.php';
+        require 'application/views/home/googleapi.php';
         require 'application/views/_templates/footer.php';
     }
 
