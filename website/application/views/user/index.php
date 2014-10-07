74464f1dc4abc3cf98b75c045458a1ce4446fdf5
Søren Skibsted Als <s_s_a_92@hotmail.com>
2014-10-07 09:06:20 +0200

    Dennis' snak bliver brugt

--- website/application/views/user/index.php
+++ website/application/views/user/index.php
@@ -1,9 +1,9 @@
-<div id="Sign-In"> 
+<div id="sign-In"> 
 	
 		<form method="POST" action="/user/login">
 			 <b>User</b><br><input type="text" name="username" size="50%"><br> 
 			 <b>Password</b><br><input type="password" name="password" size="50%"><br> 
-			 <div id="Login-Fields" >
+			 <div id="login-Fields" >
 				 <a href="#">Forgotten your password?</a>
 				 <a href="#">Create new user</a>
 				 <input id="button" type="submit" name="submit" value="Log-In"> 
