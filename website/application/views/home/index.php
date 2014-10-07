f8b29a8cad44224d32e904357cac63c32efe9575
Dennis Jakobsen <dennis.jakobsen@gmail.com>
2014-10-07 10:12:22 +0200

    Begyndt på søg + rettelser

--- website/application/views/home/index.php
+++ website/application/views/home/index.php
@@ -1,4 +1,10 @@
 <div id="map-canvas"></div>
 <div id="mapinfo">
-	
+	<div style="position: relative;">
+		<input type="text" id="searchstation" name="searchstation" class="searchbox" placeholder="Search" onkeyup="SearchStation()" /><br />
+		<div id="searchresult">
+			please work!<br />
+			test
+		</div>
+	</div>
 </div>
