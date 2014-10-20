5f289c7dc387ece1546263878c456e70733cee6c
Søren Skibsted Als <s_s_a_92@hotmail.com>
2014-10-20 13:36:28 +0200

    GPSUpdates har lavet sådan man kan køre med cykler rundt i byen

---
+++ GPSUpdatus/GPSUpdatus/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace GPSUpdatus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numUpDownBicycle = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstRoutes = new System.Windows.Forms.ListBox();
+            this.btnStartStopRoute = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownBicycle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numUpDownBicycle
+            // 
+            this.numUpDownBicycle.Location = new System.Drawing.Point(12, 39);
+            this.numUpDownBicycle.Maximum = new decimal(new int[] {
+            178,
+            0,
+            0,
+            0});
+            this.numUpDownBicycle.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numUpDownBicycle.Name = "numUpDownBicycle";
+            this.numUpDownBicycle.Size = new System.Drawing.Size(120, 20);
+            this.numUpDownBicycle.TabIndex = 0;
+            this.numUpDownBicycle.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bicycle Number";
+            // 
+            // lstRoutes
+            // 
+            this.lstRoutes.FormattingEnabled = true;
+            this.lstRoutes.Location = new System.Drawing.Point(12, 66);
+            this.lstRoutes.Name = "lstRoutes";
+            this.lstRoutes.Size = new System.Drawing.Size(260, 95);
+            this.lstRoutes.TabIndex = 2;
+            // 
+            // btnStartStopRoute
+            // 
+            this.btnStartStopRoute.Location = new System.Drawing.Point(12, 168);
+            this.btnStartStopRoute.Name = "btnStartStopRoute";
+            this.btnStartStopRoute.Size = new System.Drawing.Size(120, 23);
+            this.btnStartStopRoute.TabIndex = 3;
+            this.btnStartStopRoute.Text = "Start/Stop Route";
+            this.btnStartStopRoute.UseVisualStyleBackColor = true;
+            this.btnStartStopRoute.Click += new System.EventHandler(this.btnStartStopRoute_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnStartStopRoute);
+            this.Controls.Add(this.lstRoutes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numUpDownBicycle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownBicycle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numUpDownBicycle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstRoutes;
+        private System.Windows.Forms.Button btnStartStopRoute;
+    }
+}
+
---
+++ website/interface/gpsregister.php
@@ -0,0 +1,57 @@
+<?php
+    require_once('lib/nusoap.php'); // basic include.. must go at the top
+   
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'bicycle-db');
+    define('DB_USER', 'sw707e14');
+    define('DB_PASS', 'saledes');
+    $db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    mysqli_set_charset($db, "utf8");
+   
+   
+    $server = new soap_server(); // the soap object from the include above.
+    $SERVICE_NAMESPACE = "urn:GPS_Service"; // create a namespace to run under.
+
+    $server->soap_defencoding = 'UTF-8';
+    $server->decode_utf8 = false;
+    $server->encode_utf8 = true;
+
+    //we need to assign this somehow
+
+    // this has many input parameters but we only need two: the service name and the namespace
+    $server->configureWSDL('GPS_Service', $SERVICE_NAMESPACE);
+
+    $server->register('RegisterGPS',
+        array('bicycle_id' => 'xsd:int',
+              'latitude' => 'xsd:float',
+              'longitude' => 'xsd:float'),
+        array('return' => 'xsd:boolean'),
+        $SERVICE_NAMESPACE,
+        $SERVICE_NAMESPACE . '#soapaction',
+        'rpc',
+        'literal',
+        'Updates location of bicycle'
+    );
+    function RegisterGPS($bicycle_id, $latitude, $longitude)
+    {
+        global $db;
+        
+        $stmt = $db->prepare("UPDATE bicycle SET latitude = ?, longitude = ? WHERE bicycle_id = ?");
+        $stmt->bind_param("ddi", $latitude, $longitude, $bicycle_id);
+        $stmt->execute();
+        $stmt->close();
+        
+        return true;
+    }
+        
+   
+
+    //This processes the request and returns a result.
+    ob_clean();
+    $HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
+    $server->service(trim($HTTP_RAW_POST_DATA)); 
+
+?>
+
+
+
