156e439f1c3c9558bd11dfcee5d74faadc479e72
Winde <mapede11@student.aau.dk>
2014-10-20 14:56:55 +0200

    Ændrede cykel billede for admin map

--- website/application/views/home/adminMap.php
+++ website/application/views/home/adminMap.php
@@ -7,11 +7,11 @@
 	var bicycleimage = {
 	    url: '/public/images/bicycleMarker.png',
 	    // This marker is 20 pixels wide by 32 pixels tall.
-	    size: new google.maps.Size(9, 9),
+	    size: new google.maps.Size(10, 10),
 	    // The origin for this image is 0,0.
 	    origin: new google.maps.Point(0,0),
 	    // The anchor for this image is the base of the flagpole at 0,32.
-	    anchor: new google.maps.Point(4, 4),
+	    anchor: new google.maps.Point(5, 5),
 		};
 
 	function initialize() {
