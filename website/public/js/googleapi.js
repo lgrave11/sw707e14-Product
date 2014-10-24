e3ab9a89c8c1bb2ea0ae8998721d4c73eb3ed292
Winde <mapede11@student.aau.dk>
2014-10-24 10:23:37 +0200

    This will do!

--- website/public/js/googleapi.js
+++ website/public/js/googleapi.js
@@ -42,9 +42,31 @@ function getTheStations() {
         var stations = $.parseJSON(data);
         for(i = 0; i < stations.length; i++){
 
+var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
 
-            var info = new google.maps.InfoWindow(
-                        { content: "<div style=\"overflow:hidden;white-space:nowrap;\"><b>" + stations[i]["name"] + "</b><br/> Available Bicycles: " + freeBicycleList[i+1] + "<br/> Available Docks: " + freeDockList[i+1] + "</div>"});
+            var info = new google.maps.InfoWindow(//{ content: contentString});
+                        { content: '<div><h2 style="margin-bottom:-10px;">' + stations[i]["name"] + '</h2>' +
+                        '<div id="bodyContent"><p> Available Bicycles: '   + freeBicycleList[i+1] + 
+                        '</br> Available Docks: ' + freeDockList[i+1] + '</p></div></div>'});
             
             
             var marker = new google.maps.Marker({
