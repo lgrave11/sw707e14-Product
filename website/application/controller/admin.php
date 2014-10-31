c1daa11606356bd70d556426f0033d51277a51cd
Erik S. Jensen <ejens11@student.aau.dk>
2014-10-31 12:36:44 +0100

    Temporary for Dennis

--- website/application/controller/admin.php
+++ website/application/controller/admin.php
@@ -2,18 +2,10 @@
 
 /**
  * Class Admin
- *
- * Please note:
- * Don't use the same name for class and method, as this might trigger an (unintended) __construct of the class.
- * This is really weird behaviour, but documented here: http://php.net/manual/en/language.oop5.decon.php
- *
  */
+
 class Admin extends Controller
 {
-    /**
-     * PAGE: index
-     * This method handles what happens when you move to http://yourproject/home/index (which is the default page btw)
-     */
     public function index()
     {
         $this->title = "Admin";
@@ -30,6 +22,13 @@ class Admin extends Controller
 
         require 'application/views/_templates/adminheader.php';
         require 'application/views/admin/admin.php';
+        require 'application/views/_templates/footer.php';
+    }
+    
+    public function usageHistory() {
+        
+        require 'application/views/_templates/adminheader.php';
+        require 'application/views/admin/usagehistory.php';
         require 'application/views/_templates/footer.php';
     }
 }
