01db7d273d88b2d5aaeb5fffbb244438386cbc40
Søren Skibsted Als <s_s_a_92@hotmail.com>
2014-11-03 08:47:17 +0100

    så skulle det være merged

--- website/application/controller/admin.php
+++ website/application/controller/admin.php
@@ -2,20 +2,14 @@
 
 /**
  * Class Admin
- *
- * Please note:
- * Don't use the same name for class and method, as this might trigger an (unintended) __construct of the class.
- * This is really weird behaviour, but documented here: http://php.net/manual/en/language.oop5.decon.php
- *
  */
+
 class Admin extends Controller
 {
-    /**
-     * PAGE: index
-     * This method handles what happens when you move to http://yourproject/home/index (which is the default page btw)
-     */
     public function index()
     {
+        Tools::requireAdmin();
+        
         $this->title = "Admin";
         $currentPage = substr($_SERVER["REQUEST_URI"], 1);
 
@@ -28,11 +22,11 @@ class Admin extends Controller
 
         }
 
-        // load views. within the views we can echo out $songs and $amount_of_songs easily
-        require 'application/views/_templates/header.php';
+        require 'application/views/_templates/adminheader.php';
         require 'application/views/admin/admin.php';
         require 'application/views/_templates/footer.php';
     }
+
 
     public function mapRoutes() 
     {
@@ -48,5 +42,27 @@ class Admin extends Controller
         require 'application/views/_templates/footer.php';
     }
     
+    public function usageHistory() {
+        Tools::requireAdmin();
+        
+        $this->title = "Usage History";
+        $bicycleservice = $this->loadModel("bicycleservice");
+        $stationservice = $this->loadModel("stationservice");
+        
+        $list = array_map(function($b) { return $b->id; }, $bicycleservice->readAll());
+        
+        require 'application/views/_templates/adminheader.php';
+        require 'application/views/admin/usagehistory.php';
+        require 'application/views/_templates/footer.php';
+    }
+
+    public function graphTest(){
+        
+
+        require 'application/views/_templates/adminheader.php';
+        require 'application/views/admin/graphtest.php';
+        require 'application/views/_templates/footer.php';
+    }
+
 }
 ?>
