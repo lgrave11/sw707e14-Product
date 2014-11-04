05e9ddae1c0861053dee0a2ca755d513a6ab4dbc
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2014-11-04 09:29:41 +0100

    Random colors.

--- website/public/js/routesMap.js
+++ website/public/js/routesMap.js
@@ -40,7 +40,7 @@ function initialize() {
       strokeWeight: 2
     });
     
-    for(var i; i < positionsLatLng.length; i++) 
+    /*for(var i; i < positionsLatLng.length; i++) 
     {
         var marker = new google.maps.Marker(
                     { 
@@ -57,17 +57,55 @@ function initialize() {
         infowindow.push(iw);
         
         google.maps.event.addListener(marker, 'click', infoHelper(marker, iw, map));
-    }
+    }*/
     routePath.setMap(map);
 }
 
-function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
+function getRandomColor() 
+{
+    var h = Math.floor(Math.random() * 360) + 1;
+    var s = 75;
+    var l = 50;
+    var rgb = hslToRgb(h,s,l);
+    return rgbToHex(rgb[0], rgb[1], rgb[2]);
+    
+}
+
+function hslToRgb(h, s, l){
+    var r, g, b;
+    h = h / 360;
+    s = s / 100;
+    l = l / 100;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
     }
-    return color;
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
 function infoHelper(marker, info, map){
