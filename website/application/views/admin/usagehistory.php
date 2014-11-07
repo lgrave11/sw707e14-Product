d694548a48ffb7722189849ff5783ddab72ee68f
Erik S. Jensen <ejens11@student.aau.dk>
2014-11-07 13:38:47 +0100

    Made the history usage page somewhat pretty

--- website/application/views/admin/usagehistory.php
+++ website/application/views/admin/usagehistory.php
@@ -1,5 +1,41 @@
 <div>
     <form>
+        <table style="width:100%;">
+            <tr>
+                <td><h2>Choose Perspective</h2></td>
+                <td id="selectlistlabel" style="width:300px;"><h2>Choose Bicycle</h2></td>
+                <td><h2>From Time</h2></td>
+                <td><h2>To Time</h2></td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td>
+                    <select id="usageperspective" onchange="UpdateSelectList()">
+                        <option>Bicycle</option>
+                        <option>Station</option>
+                    </select>
+                </td>
+                <td>
+                    <select name="StationBicycleList">
+                        <?php
+                            foreach ($list as $item) {
+                                echo ViewHelper::GenerateHTMLSelectOption($item, array('value'=>$item));
+                            }
+                        ?>
+                    </select>
+                </td>
+                <td>
+                    <input id="admin_fromdatepicker" type="text" readonly name="fromdate" value="<?php echo ViewHelper::printDateTime(); ?>" style="width: 100px; text-align: center;" />
+                </td>
+                <td>
+                    <input id="admin_todatepicker" type="text" readonly name="todate" value="<?php echo ViewHelper::printDateTime(); ?>" style="width: 100px; text-align: center;" />
+                </td>
+                <td>
+                    <input type="button" onclick="UpdateUsageContent()" value="Get History" />
+                </td>
+            </tr>
+        </table>
+        <!--<div style="float: left; width: 50%; display: inline-block; text-align: center;">
         <h2>Choose Perspective</h2>
         <select id="usageperspective" onchange="UpdateSelectList()">
             <option>Bicycle</option>
@@ -12,12 +48,16 @@
                     echo ViewHelper::GenerateHTMLSelectOption($item, array('value'=>$item));
                 }
             ?>
-        </select><br /><br />
+        </select>
+        </div>
+        <div style="float: right; width: 50%; display: inline-block; text-align: center;">
         <?php
             require 'application/views/admin/timeintervalpicker.php';
         ?>
-        <br /><br />
+        </div>
+        <center style="clear: both;">
         <input type="button" onclick="UpdateUsageContent()" value="Get History" />
+        </center>-->
     </form>
     <hr />
     <div>
