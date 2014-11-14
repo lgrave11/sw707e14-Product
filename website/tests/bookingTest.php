05fffe57b73b11fab000efbc95b172c1d527c4bb
Winde <mapede11@student.aau.dk>
2014-11-14 13:42:42 +0100

    We like to party

--- website/tests/bookingTest.php
+++ website/tests/bookingTest.php
@@ -211,10 +211,9 @@ class BookingTest extends PHPUnit_Framework_TestCase
 
 
 		//Cleanup
-		$accountservice->delete($account);
+		$stationservice->deleteForTest($station);
 		$bookingservice->delete($booking);
-		$stationservice->delete($station);
-
+		$accountservice->delete($account);
 	}
 
 
@@ -260,12 +259,12 @@ class BookingTest extends PHPUnit_Framework_TestCase
 		$this->assertCount(3, $mappedresult2);
 
 		//Cleanup
-		$accountservice->delete($account1);
-		$accountservice->delete($account2);
 		$bookingservice->delete($booking1);
 		$bookingservice->delete($booking2);
 		$bookingservice->delete($booking3);
 		$bookingservice->delete($booking4);
+		$accountservice->delete($account1);
+		$accountservice->delete($account2);
 
 	}
 
@@ -313,12 +312,12 @@ class BookingTest extends PHPUnit_Framework_TestCase
 		$this->assertCount(3, $mappedresult2);
 
 		//Cleanup
-		$accountservice->delete($account1);
-		$accountservice->delete($account2);
 		$bookingservice->delete($booking1);
 		$bookingservice->delete($booking2);
 		$bookingservice->delete($booking3);
 		$bookingservice->delete($booking4);
+		$accountservice->delete($account1);
+		$accountservice->delete($account2);
 
 	}
 
@@ -366,14 +365,13 @@ class BookingTest extends PHPUnit_Framework_TestCase
 		$this->assertCount(3, $mappedresult2);
 
 		//Cleanup
-		$accountservice->delete($account1);
-		$accountservice->delete($account2);
 		$bookingservice->delete($booking1);
 		$bookingservice->delete($booking2);
 		$bookingservice->delete($booking3);
 		$bookingservice->delete($booking4);
 		$bookingservice->delete($booking5);
-
+		$accountservice->delete($account1);
+		$accountservice->delete($account2);
 	}
 
 
