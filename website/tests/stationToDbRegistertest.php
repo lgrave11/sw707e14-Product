87a14dfa5d21ec1936f5df8621896c8c72a60ba2
Søren Skibsted Als <s_s_a_92@hotmail.com>
2014-11-24 15:55:34 +0100

    Så fik vi det til at virke.

--- website/tests/stationToDbRegistertest.php
+++ website/tests/stationToDbRegistertest.php
@@ -13,10 +13,14 @@ class StationToDbRegisterTest extends PHPUnit_Framework_TestCase
 
     public function testBicycleWithBookingUnlocked() 
     {
+        global $db;
         $bookingService = new BookingService($this->db);
         $bicycle_id = 1;
         $station_id = 1;
         $time = time();
+        $stmt = $db->prepare("DELETE FROM booking WHERE password = 123456 AND for_user = 'sw707e14'");
+        $stmt->execute();
+        $stmt->close();
         $booking = new Booking(0, $time, $station_id, "123456", "sw707e14");
         $booking = $bookingService->create($booking);
         $this->assertTrue(BicycleWithBookingUnlocked($station_id, $booking->booking_id, $bicycle_id));
@@ -27,12 +31,13 @@ class StationToDbRegisterTest extends PHPUnit_Framework_TestCase
         $bookingService->delete($bookingRead);
     }
     
+
     public function testBicycleTaken(){
     	global $db;
     	$db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     	mysqli_set_charset($db, "utf8");
         $dockService = new DockService($this->db);
-        $histroyUsageBicycleService = new HistoryUsageBicycleService($this->db);
+        $historyUsageBicycleService = new HistoryUsageBicycleService($this->db);
         $bookingService = new BookingService($this->db);
 
         $station_id = 1;
@@ -46,18 +51,31 @@ class StationToDbRegisterTest extends PHPUnit_Framework_TestCase
         $booking = new Booking(0, $time, $station_id, "123456", "sw707e14");
         $booking = $bookingService->create($booking);
 
-        $this->assertTrue(BicycleTaken($station_id, $bicycle_id, $booking->booking_id));
+        $stmt = $db->prepare("SELECT COUNT(*) FROM historyusagebicycle WHERE bicycle_id = ? AND start_station = ? AND start_time BETWEEN ? AND ?");
+        $prevtime = $time - 2;
+        $nexttime = $time + 2;
+        $stmt->bind_param("iiii", $bicycle_id, $station_id, $prevtime, $nexttime);
+        $stmt->execute();
+        $stmt->bind_result($oldcount);
+        $stmt->fetch();
+        $stmt->close();
 
+        $this->assertTrue(BicycleTaken($station_id, $bicycle_id, $booking->booking_id));
         
         $dockRead = $dockService->read($dock_id);
 
         $this->assertEquals(NULL, $dockRead->holds_bicycle);
 
-        $historyUsage = $histroyUsageBicycleService->readHistoryBetween($time - 1, $time + 1);
+        $stmt = $db->prepare("SELECT COUNT(*) FROM historyusagebicycle WHERE bicycle_id = ? AND start_station = ? AND start_time BETWEEN ? AND ?");
+        $prevtime = $time - 2;
+        $nexttime = $time + 2;
+        $stmt->bind_param("iiii", $bicycle_id, $station_id, $prevtime, $nexttime);
+        $stmt->execute();
+        $stmt->bind_result($newcount);
+        $stmt->fetch();
+        $stmt->close();
 
-        $this->assertEquals($historyUsage[0]->bicycle_id, $bicycle_id);
-        $this->assertEquals($historyUsage[0]->start_station, $station_id);
-        $this->assertEquals($historyUsage[0]->booking_id, $booking->booking_id);
+        $this->assertEquals($oldcount + 1, $newcount);
 
         // 
         $this->assertTrue(BicycleTaken($station_id, $bicycle_id));
