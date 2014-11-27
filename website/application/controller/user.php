142ab87d011f7ef550fb5fa461b5e03bad34f8bb
Winde <mapede11@student.aau.dk>
2014-11-27 15:46:56 +0100

    En masse bawl

--- website/application/controller/user.php
+++ website/application/controller/user.php
@@ -86,6 +86,7 @@ class User extends Controller
                         
                         
                         if ($target=="") {
+                            $this->success("You are now logged in as " . $_POST['username'], "home");
                             header("Location: /");
                             exit();
                         }
